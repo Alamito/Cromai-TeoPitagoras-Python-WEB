b2d9aa72d25401a990d2b6e576781f2cee2889a0
Alamito <alamirbobroski12@gmail.com>
2023-01-20 21:18:55 -0300

    feat - calculate and print value

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 const HIP = document.querySelector('.HIP');
 const sizeA = document.querySelector('.size-A');
 const sizeB = document.querySelector('.size-B');
+const button = document.querySelector('.buttonCalculate');
 
-document.addEventListener('keydown', (event) => {
-   if (event.key === 'Enter') {
-      // console.log(sizeA.value, sizeB.value)
-      getValue(HIP.value, sizeA.value, sizeB.value)
+document.addEventListener('click', (event) => {
+   if (event.target === button) {
+      const valueHIP = checkValueAndAddCaractere(HIP.value);
+      // console.log(valueHIP)
+      const valueA = checkValueAndAddCaractere(sizeA.value);
+      const valueB = checkValueAndAddCaractere(sizeB.value);
+      getValue(valueHIP || "0.0", valueA || "0.0", valueB || "0.0")
    }
 });
 
-const getValue = (HIP = 0.0, sizeA = 0.0, sizeB = 0.0) => {
+const checkValueAndAddCaractere = (value) => {
+   if (!value) {
+      return null;
+   }
+   if (value.indexOf('.') === -1) {
+      return value + '.0';
+   } else {
+      return value + '0';
+   }
+}
+
+const getValue = (HIP, sizeA, sizeB) => {
    const URLWithValues = `http://localhost:5000/triangleSides/${HIP};${sizeA};${sizeB}`;
    axios.get(URLWithValues)
       .then(response => {
-         const data = response.data;
-         console.log(data)
+         return response.data;
       })
+      .then(data => {
+         printValue(data.size, data.value);
+   })
 }
 
-
+const printValue = (size, value) => {
+   if (size === "hip") {
+      HIP.value = value;
+   } else if (size === "A") {
+      sizeA.value = value;
+   } else {
+      sizeB.value = value;
+   }
+}
 
 
 function onlyNumber(evt) {
