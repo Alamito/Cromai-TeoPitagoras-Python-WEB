8972f7ba8a9bd568246bc3a62229df547c1cd0b2
Alamito <alamirbobroski12@gmail.com>
2023-01-22 11:55:04 -0300

    fix - cors problem solved

--- API/app.py
+++ API/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import math
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/triangleSides/<float:hip>;<float:sizeA>;<float:sizeB>', methods=['GET'])
 def getAll(hip, sizeA, sizeB):
